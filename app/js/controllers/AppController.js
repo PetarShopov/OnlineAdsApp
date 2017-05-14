a38c4283723d3d5dbe6c6c7e88e82d607e2862f9
@PetarShopov <p.shopov@gmail.com>
2017-05-14 11:26:09 +0300

    Added app ctrl, header and left sidebar temp

--- app/js/controllers/AppController.js
+++ app/js/controllers/AppController.js
@@ -1,9 +1,17 @@
 'use strict';
 
 app.controller('AppController', [
-        '$scope',
-        'authService',
-        function ($scope, authService) {
-            // Put the authService in the $scope to make it accessible from all screens
-            $scope.authService = authService;
-        }]);
+    '$scope',
+    '$location',
+    'authService',
+    'notifyService',
+    function ($scope, $location, authService, notifyService) {
+        // Put the authService in the $scope to make it accessible from all screens
+        $scope.authService = authService;
+
+        $scope.logout = function () {
+            authService.logout();
+            notifyService.showInfo("Logout successful");
+            $location.path('/');
+        };
+    }]);
