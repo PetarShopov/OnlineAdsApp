75c11a30ada0ce68b455a8fe4472240a85eab514
@PetarShopov <p.shopov@gmail.com>
2017-05-14 11:46:23 +0300

    Add registration

--- app/js/controllers/RegisterController.js
+++ app/js/controllers/RegisterController.js
@@ -3,9 +3,26 @@
 
 app.controller('RegisterController', [
     '$scope',
+    '$rootScope',
     '$location',
+    'townsService',
     'authService',
     'notifyService',
-    function ($scope, $location, authService, notifyService) {
-        // TODO
+    function ($scope, $rootScope, $location, townsService, authService, notifyService) {
+        $rootScope.pageTitle = "Register";
+
+        $scope.userData = {townId: null};
+        $scope.towns = townsService.getTowns();
+
+        $scope.register = function(userData) {
+            authService.register(userData,
+                function success() {
+                    notifyService.showInfo("User registered successfully");
+                    $location.path("/");
+                },
+                function error(err) {
+                    notifyService.showError("User registration failed", err);
+                }
+            );
+        };
     }]);
